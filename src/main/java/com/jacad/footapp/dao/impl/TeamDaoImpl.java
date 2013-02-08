676558095354cc2eb8cc71895735ad9408974db1
Matthis <matthisenforme@gmail.com>
2013-02-08 22:58:57 +0100

    Modif d'un fichier oublié

--- src/main/java/com/jacad/footapp/dao/impl/TeamDaoImpl.java
+++ src/main/java/com/jacad/footapp/dao/impl/TeamDaoImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.jacad.footapp.dao.TeamDao;
-import com.jacad.footapp.domain.Player;
 import com.jacad.footapp.domain.Team;
 
 @Repository
